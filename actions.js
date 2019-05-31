21642c132d97987fc32a479904e6591246627472
Alyssa Jane <xcthaeh@gmail.com>
2019-05-31 11:43:57 -0400

    added a post request for actions

--- actions.js
+++ actions.js
@@ -2,28 +2,46 @@ const express = require("express");
 const cors = require("cors");
 const actionModel = require("./data/helpers/actionModel.js");
 
-  server.get('/actions', (req, res) => {
+server.get("/actions", (req, res) => {
+  actionModel
+    .get()
+    .then(actions => {
+      res.status(200).json(actions);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+server.get("/actions/:id", (req, res) => {
+  actionModel
+    .get(req.params.id)
+    .then(action => {
+      res.status(200).json(action);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+server.post("/actions", (req, res) => {
+  const { project_id, description, notes } = req.body;
+  const newAction = { project_id, description, notes };
+
+  if (!newAction) {
+    res.status(400).json({ errorMessage: "Required field" });
+  } else {
     actionModel
-      .get()
-      .then(actions => {
-        res.status(200).json(actions);
+      .insert(newAction)
+      .then(newActionRes => {
+        res.status(201).json({ post: "New action was added." });
       })
       .catch(err => {
-        res.status(500).json(err);
+        res.send(err);
       });
-  })
-  
-  server.get('/actions/:id', (req, res) => {
-    actionModel
-      .get(req.params.id)
-      .then(action => {
-        res.status(200).json(action);
-      })
-      .catch(err => {
-        res.status(500).json(err);
-      });
-  })
-  
-  
-  
-  module.exports = server;
+  }
+});
+
+
+
+module.exports = server;
