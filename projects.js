971f0ee021a681c7a254787ce2ce2697a45798bc
Alyssa Jane <xcthaeh@gmail.com>
2019-05-31 11:24:47 -0400

    added get and post requests for projects, created an actions file.

---
+++ projects.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const cors = require("cors");
+const projectModel = require("./data/helpers/projectModel.js");
+
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+server.get("/projects", (req, res) => {
+  projectModel
+    .get()
+    .then(projects => {
+      res.status(200).json(projects);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+server.get("/projects/:id", (req, res) => {
+  projectModel
+    .get(req.params.id)
+    .then(project => {
+      res.status(200).json(project);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+server.post("/projects", (req, res) => {
+  const { name, description } = req.body;
+  const newProject = { name, description };
+
+  if (!newProject) {
+    res.status(400).json({ errorMessage: "Required field" });
+  } else {
+    projectModel
+      .insert(newProject)
+      .then(newProjectRes => {
+        res.status(201).json({ posted: newProjectRes });
+      })
+      .catch(err => {
+        res.send(err);
+      });
+  }
+});
